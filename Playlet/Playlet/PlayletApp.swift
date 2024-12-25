ba7c16e547da3c952340dd0225461e4d52b32ade
helinyu3 <2319979647@qq.com>
2024-12-25 17:16:47 +0800

    init

---
+++ Playlet/Playlet/PlayletApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayletApp.swift
+//  Playlet
+//
+//  Created by waaw on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlayletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
